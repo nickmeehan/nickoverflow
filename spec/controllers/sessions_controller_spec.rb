c17c5056bd49e51eaff0b9ed32605a40b38b8a3b
Nick Meehan <nnmeehan@gmail.com>
2014-05-17 14:18:26 -0700

    #new route tested in sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -7,4 +7,20 @@ describe SessionsController do
       expect(response).to be_success
     end
   end
+
+  context "#create" do
+    let(:user) { FactoryGirl.create(:user) }
+    it "sets session id to user id" do
+      params = { email: user.email, password: user.password }
+      post :create, params
+      expect(session[:user_id]).to eq user.id
+      expect(response).to be_redirect
+    end
+
+    it "does not let user sign in if credentials are incorrect" do
+      params = { email: "myemail", password: "mypassword" }
+      post :create, params
+      expect(response).to_not be_redirect
+    end
+  end
 end
