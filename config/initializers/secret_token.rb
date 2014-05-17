d4598f05e0eb0c254c4e72101ad8534190e90553
Nick Meehan <nnmeehan@gmail.com>
2014-05-17 12:13:05 -0700

    Added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nickoverflow::Application.config.secret_token = '7d49082807c28bb0511957601b5541fe58a5c12a6ad2609d6c9460b001bf67d4c26ad5d38c53f3e87c365108d25784d3b460e37aca518307da6f303cb66c811c'
