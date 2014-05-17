f67b82d6984d066521e4277d25b5de6b3edea4ab
Nick Meehan <nnmeehan@gmail.com>
2014-05-17 16:20:35 -0700

    #show assigns both correct question and author_id. #new response is successful

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,13 +4,31 @@ describe QuestionsController do
 
   context "#show" do
     let(:question) { FactoryGirl.create :question }
+    let(:author) { question.user }
     it "is successful" do
       params = { id: question.id }
       get :show, params
       expect(response).to be_success
     end
+
+    it "assigns question to the correct Question" do
+      params = { id: question.id }
+      get :show, params
+      expect(assigns(:question)).to eq question
+    end
+
+    it "assigns correct id to author" do
+      params = { id: question.id }
+      get :show, params
+      expect(assigns(:author_id)).to eq author.id
+    end
   end
 
-
+  context "#new" do
+    it "is successful" do
+      get :new
+      expect(response).to be_success
+    end
+  end
 
 end
