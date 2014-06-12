798ee624e8e409b624d5531144e55539f9e3f125
Nick Meehan <nnmeehan@gmail.com>
2014-06-12 13:40:14 -0700

    Changes made to schema reflected from new migration added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140611201853) do
+ActiveRecord::Schema.define(:version => 20140612185037) do
 
   create_table "answers", :force => true do |t|
     t.text     "content"
     t.integer  "user_id"
     t.integer  "question_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+    t.integer  "vote_count",  :default => 0
   end
 
   create_table "comments", :force => true do |t|
