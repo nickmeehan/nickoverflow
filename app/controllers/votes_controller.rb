1f84a3fa9aefa5a3e10955b0c216db93fb2111f1
Nick Meehan <nnmeehan@gmail.com>
2014-06-12 10:51:55 -0700

    Added logic for Votes Controller to handle saving a new vote, or updating an existing vote.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,18 +10,31 @@ class VotesController < ApplicationController
 	end
 
 	def create
-		new_vote = Vote.verify_as_new(params[:vote])
+		new_vote = Vote.verify_as_new(params[:vote], session[:user_id])
 		user = User.find(session[:user_id])
 		@vote = user.votes.build(params[:vote])
-		p new_vote
-		p params
-		p @vote
+		if new_vote && @vote.save
+			p new_vote
+			p params
+			p @vote
+			render json: { voted: "what" }
+		else
+			update
+		end
 	end
 
 	def edit
 	end
 
 	def update
+		@vote = Vote.find_vote(params[:vote], session[:user_id])
+		if @vote.update_attributes(params[:vote])
+			p @vote
+		else
+			p "WHAAATTTTT UPPP!!!"
+			p params
+			p @vote
+		end
 	end
 
 	def destroy
