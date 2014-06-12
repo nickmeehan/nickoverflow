98f328202d312b80e33d74f164214d8053bd7d98
Nick Meehan <nnmeehan@gmail.com>
2014-06-12 10:53:06 -0700

    Added logic on how to determine whether a vote is new, and retrieve a particular vote based on answer id and user id.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,9 +4,19 @@ class Vote < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :votable, polymorphic: true
 
-	def self.verify_as_new(vote_params)
+	def self.verify_as_new(vote_params, voter_id)
 		if vote_params["votable_type"] == "Answer"
 			answer = Answer.find(vote_params["votable_id"])
+			existing_user = answer.votes.find_by_user_id(voter_id)
+			existing_user == nil ? true : false
+		end
+	end
+
+	def self.find_vote(vote_params, voter_id)
+		if vote_params["votable_type"] == "Answer"
+			answer = Answer.find(vote_params["votable_id"])
+			vote = answer.votes.find_by_user_id(voter_id)
+			return vote
 		end
 	end
 
