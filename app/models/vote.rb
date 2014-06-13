c21fac859053cea1067af3fef453c81ea72df19c
Nick Meehan <nnmeehan@gmail.com>
2014-06-13 16:38:27 -0700

    Added logic to not show comments or votes for when users are not logged in.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
 	
 	belongs_to :user
 	belongs_to :votable, polymorphic: true
+	validates :user_id, presence: true
 	validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
 
 	def self.find_vote(vote_params)
