bd1c01cddb1715d445699f67c5b2f07c89b28292
Nick Meehan <nnmeehan@gmail.com>
2014-06-13 14:34:31 -0700

    Added event delegation for commenting on answers and questions in each of the respective controllers.

--- app/assets/javascripts/comments/answer_comments_controller.js
+++ app/assets/javascripts/comments/answer_comments_controller.js
@@ -7,9 +7,9 @@ AnswerCommentsController.prototype = {
     this.bindListeners();
   },
   bindListeners: function() {
-    var $answerCommentSelector = this.view.getCommentSelector();
-    $answerCommentSelector.on('ajax:success', this.view.appendNewComment.bind(this.view))
-    $answerCommentSelector.on('ajax:error', this.view.appendNewCommentErrors.bind(this.view))
+    var $container = ContainerSelector.retrieve();
+    $container.on('ajax:success', this.view.commentSelector,this.view.appendNewComment.bind(this.view))
+    $container.on('ajax:error', this.view.commentSelector,this.view.appendNewCommentErrors.bind(this.view))
     var $commentDeletionSelector = this.view.getCommentDeletionSelector();
     $commentDeletionSelector.on('ajax:success', this.view.removeComment.bind(this.view))
     $commentDeletionSelector.on('ajax:error', this.view.displayCommentErrors.bind(this.view))
--- app/assets/javascripts/comments/question_comments_controller.js
+++ app/assets/javascripts/comments/question_comments_controller.js
@@ -7,9 +7,9 @@ QuestionCommentsController.prototype = {
     this.bindListeners();
   },
   bindListeners: function() {
-    var questionCommentSelector = this.view.getCommentSelector();
-    questionCommentSelector.on('ajax:success', this.view.appendNewComment.bind(this.view))
-    questionCommentSelector.on('ajax:error', this.view.appendNewCommentErrors.bind(this.view))
+    var $container = ContainerSelector.retrieve();
+    $container.on('ajax:success', this.view.commentSelector,this.view.appendNewComment.bind(this.view))
+    $container.on('ajax:error', this.view.commentSelector,this.view.appendNewCommentErrors.bind(this.view))
     var commentDeletionSelector = this.view.getCommentDeletionSelector();
     commentDeletionSelector.on('ajax:success', this.view.removeComment.bind(this.view))
     commentDeletionSelector.on('ajax:error', this.view.displayCommentErrors.bind(this.view))
