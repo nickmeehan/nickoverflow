794c4047b6495813986b32d89e6e3fa1468826d1
Nick Meehan <nnmeehan@gmail.com>
2014-06-13 11:59:40 -0700

    Rolled back migration adding vote_count column to answers, and removed corresponding migration.

--- db/migrate/20140612185037_add_vote_count_to_answers.rb
+++
@@ -1,9 +0,0 @@
-class AddVoteCountToAnswers < ActiveRecord::Migration
-  def up
-  	add_column :answers, :vote_count, :integer, default: 0
-  end
-
-  def down
-  	remove_column :answers, :vote_count
-  end
-end
