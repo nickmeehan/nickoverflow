8a46da7f4c72755c03f61b92228da06c2b6abbd9
Nick Meehan <nnmeehan@gmail.com>
2014-06-12 19:17:21 -0700

    Added prototype for answer votes view.

--- app/assets/javascripts/votes/answer_votes_view.js
+++ app/assets/javascripts/votes/answer_votes_view.js
@@ -1,3 +1,24 @@
 function AnswerVotesView() {
-	
+	this.containerSelector = '.container';
+	this.voteSelector = '.answer_votes';
+	this.openingDataKeySelector = '[data-answer=';
+	this.closingDataKeySelector = ']';
+	this.voteCountSelector = '.vote_count'
+}
+
+AnswerVotesView.prototype = {
+	getContainerSelector: function() {
+		return $(this.containerSelector);
+	},
+	getVoteSelector: function() {
+		return $(this.voteSelector)
+	},
+	getVoteCountHolder: function(targetId) {
+		return $(this.openingDataKeySelector + targetId + this.closingDataKeySelector + this.voteSelector + ' ' + this.voteCountSelector)
+	},
+	updateVoteCount: function(event, data) {
+		console.log(data)
+		var $voteCountHolder = this.getVoteCountHolder(data.voted_on.id)
+		$voteCountHolder.html(data.voted_on.vote_count)
+	}
 }
