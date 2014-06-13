49cb2a71b102795689d89d036068f7e21c39cd67
Nick Meehan <nnmeehan@gmail.com>
2014-06-13 13:34:28 -0700

    Removed unneeded logic from vote model, now taken care of by built in active record functionality.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,21 +13,4 @@ class Vote < ActiveRecord::Base
 		end
 	end
 
-	def self.determine_newly_voted_object(vote)
-		if vote.votable_type == "Answer"
-			return Answer.find(vote.votable_id)
-		end
-	end
-
-	def self.determine_updated_vote_object(vote)
-		if vote.votable_type == "Answer"
-			answer = Answer.find(vote.votable_id)
-			downvotes = answer.votes.where(downvoted: true).count
-			upvotes = answer.votes.where(upvoted: true).count
-			answer.vote_count = upvotes - downvotes
-			answer.save
-			return answer
-		end
-	end
-
 end
