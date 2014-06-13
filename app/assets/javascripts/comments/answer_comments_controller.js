d018a981294a95a3549f9dd24d4d959fdae42eac
Nick Meehan <nnmeehan@gmail.com>
2014-06-13 16:01:59 -0700

    Added event delegation for ajax calls to delete comments. And redefined a return in comments view prototype to return a selector instead of a jQuery object.

--- app/assets/javascripts/comments/answer_comments_controller.js
+++ app/assets/javascripts/comments/answer_comments_controller.js
@@ -11,7 +11,7 @@ AnswerCommentsController.prototype = {
     $container.on('ajax:success', this.view.commentSelector,this.view.appendNewComment.bind(this.view))
     $container.on('ajax:error', this.view.commentSelector,this.view.appendNewCommentErrors.bind(this.view))
     var $commentDeletionSelector = this.view.getCommentDeletionSelector();
-    $commentDeletionSelector.on('ajax:success', this.view.removeComment.bind(this.view))
-    $commentDeletionSelector.on('ajax:error', this.view.displayCommentErrors.bind(this.view))
+    $container.on('ajax:success', $commentDeletionSelector, this.view.removeComment.bind(this.view))
+    $container.on('ajax:error', $commentDeletionSelector, this.view.displayCommentErrors.bind(this.view))
   }
 }
--- app/assets/javascripts/comments/question_comments_controller.js
+++ app/assets/javascripts/comments/question_comments_controller.js
@@ -10,8 +10,9 @@ QuestionCommentsController.prototype = {
     var $container = ContainerSelector.retrieve();
     $container.on('ajax:success', this.view.commentSelector,this.view.appendNewComment.bind(this.view))
     $container.on('ajax:error', this.view.commentSelector,this.view.appendNewCommentErrors.bind(this.view))
-    var commentDeletionSelector = this.view.getCommentDeletionSelector();
-    commentDeletionSelector.on('ajax:success', this.view.removeComment.bind(this.view))
-    commentDeletionSelector.on('ajax:error', this.view.displayCommentErrors.bind(this.view))
+    var $commentDeletionSelector = this.view.getCommentDeletionSelector();
+    $container.on('ajax:success', $commentDeletionSelector, this.view.removeComment.bind(this.view))
+    $container.on('ajax:error', $commentDeletionSelector, this.view.displayCommentErrors.bind(this.view))
+
   }
 }
