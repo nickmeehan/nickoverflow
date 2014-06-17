a8c0a24c6501c55b944cfd6d8a266f3e725fec0e
Nick Meehan <nnmeehan@gmail.com>
2014-06-16 22:47:46 -0700

    Added questions controller and view.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
   new AnswersController(new AnswersView()).init();
   new QuestionCommentsController(new QuestionCommentsView()).init();
   new AnswerCommentsController(new AnswerCommentsView()).init();
+  new QuestionVotesController(new QuestionVotesView()).init();
   new AnswerVotesController(new AnswerVotesView()).init();
 })
